1392822c0abb285a45a97313aa6ed83c6ea0e98a
AleksBobic <bobicaleks@yahoo.com>
2019-10-09 22:54:31 +0200

    refactor NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,4 +1,5 @@
-<header>
+<template>
+  <header>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <a class="navbar-brand" href="#">Carousel</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,3 +24,4 @@
     </div>
   </nav>
 </header>
+</template>
